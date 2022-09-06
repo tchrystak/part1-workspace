66f89ca5793391279921045d56781b8aece8762a
tchrystak <chrystaktrey@gmail.com>
2022-09-06 10:57:06 -0700

    Added verifyInternetConnection.

--- Lab02.2/src/Television.java
+++ Lab02.2/src/Television.java
@@ -1,12 +1,38 @@
 public class Television {
 
-  String brand;
-  int volume;
+  private String brand;
+  private int volume;
 
-  void turnOn() {
+  public void turnOn() {
+    boolean isConnected = verifyInternetConnection();
     System.out.println("The " + brand + " television is on, with the volume of " + volume);
   }
-  void turnOff() {
+
+  public void turnOff() {
     System.out.println("The " + brand + " television is off");
+  }
+
+  public String getBrand() {
+    return brand;
+  }
+
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
+
+  public int getVolume() {
+    return volume;
+  }
+
+  public void setVolume(int volume) {
+    this.volume = volume;
+  }
+
+  private boolean verifyInternetConnection() {
+    return true;
+  }
+
+  public String toString() {
+   return "Television: brand=" + getBrand() + ", volume=" + getVolume();
   }
 }
--- Lab02.2/src/TelevisionClient.java
+++ Lab02.2/src/TelevisionClient.java
@@ -2,19 +2,23 @@ public class TelevisionClient {
 
   public static void main(String[] args) {
     Television tv1 = new Television();
-    tv1.brand = "Zenith";
-    tv1.volume = 25;
+    tv1.setBrand("Zenith");
+    tv1.setVolume(25);
 
 
     Television tv2 = new Television();
-    tv2.brand = "Sony";
-    tv2.volume = 99;
+    tv2.setBrand("Sony");
+    tv2.setVolume(99);
 
     tv1.turnOn();
     tv1.turnOff();
 
     tv2.turnOn();
     tv2.turnOff();
+
+    System.out.println(tv1.toString());
+    System.out.println(tv1);
   }
+
 
 }
